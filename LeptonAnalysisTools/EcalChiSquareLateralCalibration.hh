6370363c4bc609b93dde636331ac1c01bc8cb205
Abdcefgnfenknfejf <78347028+Abdcefgnfenknfejf@users.noreply.github.com>
2022-01-07 14:33:23 +0100

    Delete LeptonAnalysisTools directory

--- LeptonAnalysisTools/EcalChiSquareLateralCalibration.hh
+++
@@ -1,8 +0,0 @@
-#ifndef EcalChiSquareLateralCalibration_hh
-#define EcalChiSquareLateralCalibration_hh
-
-#include <Rtypes.h>
-
-float EcalChiSquareLateralNormalizedCorrected(float nchi2, float ecalEnergyElectron, bool isMC);
-
-#endif
--- LeptonAnalysisTools/MagneticField.hh
+++
@@ -1,7 +0,0 @@
-#ifndef MagneticField_hh
-#define MagneticField_hh
-
-void InitializeMagneticField();
-void MagneticField(float* location, float* B);
-
-#endif
