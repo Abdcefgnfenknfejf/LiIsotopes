d6908390280d5bbe59414ca285efd9c3f34d8865
Abdcefgnfenknfejf <78347028+Abdcefgnfenknfejf@users.noreply.github.com>
2022-01-07 14:33:51 +0100

    Delete LithiumAnalysisTools directory

--- LithiumAnalysisTools/EcalBDTEfficiencyParameterization.hh
+++
@@ -1,7 +0,0 @@
-#ifndef EcalBDTEfficiencyParameterization_hh
-#define EcalBDTEfficiencyParameterization_hh
-
-class TF1;
-TF1* EcalBDTEfficiencyParameterization();
-
-#endif
--- LithiumAnalysisTools/MeasuringTimeParameterization.hh
+++
@@ -1,6 +0,0 @@
-#ifndef MeasuringTimeParameterization_hh
-#define MeasuringTimeParameterization_hh
-
-double MeasuringTimeParameterizationFunction(double* x, double* par);
-
-#endif
